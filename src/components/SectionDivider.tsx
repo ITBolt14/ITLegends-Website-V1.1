c29f152de7b9aca43fdae50c7204bc1e77b4906a
ITBolt12 <bolt12@itlegends.co.za>
2025-11-19 08:32:23 +0200

    Updated SectionDivider.tsx

---
+++ src/components/SectionDivider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionDivider: React.FC = () => {
+  return (
+    <div classname="w=full flex justify-center py-8">
+    <div classname="w-[90%] h-px bg-gradient-to-r from-itred to-itblue" />
+    </div>
+  );
+};
+
+export default SectionDivider;
